bd05d044c4a20cfac08c2ae6c7a6af0ad1d9f6d5
Anatol Pomozov <anatol.pomozov@gmail.com>
2021-03-03 10:56:55 -0800

    Drop linux keychain usage
    dmcrypt uses keychain as a way to pass keys between different processes
    safely. In this case the process command line does not contain secret
    key but instead a keyid is used.
    
    In case of luks.go we use the key inside the process only.
    
    Do not use Linux keychain but pass the key directly to devmapper.go

--- dm.go
+++ dm.go
@@ -1,25 +1,15 @@
 package luks
 
 import (
+	"encoding/hex"
 	"fmt"
 	"strings"
 
 	"github.com/anatol/devmapper.go"
-	"golang.org/x/sys/unix"
 )
 
 func createDmDevice(path string, dmName string, partitionUuid string, volume *volumeInfo, flags []string) error {
-	// load key into keyring
-	keyname := fmt.Sprintf("cryptsetup:%s-d%d", partitionUuid, volume.digestId) // get_key_description_by_digest
-	kid, err := unix.AddKey("logon", keyname, volume.key, unix.KEY_SPEC_THREAD_KEYRING)
-	if err != nil {
-		return err
-	}
-	clearSlice(volume.key)
-	defer unlinkKey(kid)
-
 	uuid := fmt.Sprintf("CRYPT-%v-%v-%v", volume.luksType, strings.ReplaceAll(partitionUuid, "-", ""), dmName) // See dm_prepare_uuid()
-	keyid := fmt.Sprintf(":%v:logon:%v", len(volume.key), keyname)
 
 	kernelFlags := make([]string, 0, len(flags))
 	for _, f := range flags {
@@ -30,29 +20,20 @@ func createDmDevice(path string, dmName string, partitionUuid string, volume *vo
 		kernelFlags = append(kernelFlags, flag)
 	}
 
+	// the key should have hex format
+	key := make([]byte, hex.EncodedLen(len(volume.key)))
+	hex.Encode(key, volume.key)
+	defer clearSlice(key)
+
 	c := devmapper.CryptTable{
 		StartSector:   0,
 		Length:        volume.storageSize,
 		BackendDevice: path,
 		BackendOffset: int(volume.storageOffset),
 		Encryption:    volume.storageEncryption,
-		Key:           keyid,
+		Key:           string(key),
 		IVTweak:       int(volume.storageIvTweak),
 		Flags:         kernelFlags,
 	}
 	return devmapper.CreateAndLoad(dmName, uuid, c)
-}
-
-func unlinkKey(kid int) {
-	if _, err := unix.KeyctlInt(unix.KEYCTL_REVOKE, kid, 0, 0, 0); err != nil {
-		fmt.Printf("key revoke: %v\n", err)
-	}
-
-	if _, err := unix.KeyctlInt(unix.KEYCTL_UNLINK, kid, unix.KEY_SPEC_THREAD_KEYRING, 0, 0); err != nil {
-		fmt.Printf("key unlink, thread: %v\n", err)
-	}
-
-	// We added key to thread keyring only. But let's try to unlink the key from other keyrings as well just to be safe
-	_, _ = unix.KeyctlInt(unix.KEYCTL_UNLINK, kid, unix.KEY_SPEC_PROCESS_KEYRING, 0, 0)
-	_, _ = unix.KeyctlInt(unix.KEYCTL_UNLINK, kid, unix.KEY_SPEC_USER_KEYRING, 0, 0)
 }
